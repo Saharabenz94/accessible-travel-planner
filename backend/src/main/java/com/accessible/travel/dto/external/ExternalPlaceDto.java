81e1496d2ef569690f13ae83d1acf0157320ef5b
Saharabenz94 <168683903+Saharabenz94@users.noreply.github.com>
2026-03-13 11:11:52 -0400

    Add ExternalPlaceDto class with properties and methods

--- backend/src/main/java/com/accessible/travel/dto/external/ExternalPlaceDto.java
+++ backend/src/main/java/com/accessible/travel/dto/external/ExternalPlaceDto.java
@@ -1,1 +1,126 @@
+package com.accessible.travel.dto.external;
 
+public class ExternalPlaceDto {
+
+    private String name;
+    private String type;
+    private String address;
+    private String city;
+    private String country;
+    private Double latitude;
+    private Double longitude;
+    private String description;
+    private String source;
+
+    public ExternalPlaceDto() {
+    }
+
+    public ExternalPlaceDto(
+            String name,
+            String type,
+            String address,
+            String city,
+            String country,
+            Double latitude,
+            Double longitude,
+            String description,
+            String source
+    ) {
+        this.name = name;
+        this.type = type;
+        this.address = address;
+        this.city = city;
+        this.country = country;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.description = description;
+        this.source = source;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    @Override
+    public String toString() {
+        return "ExternalPlaceDto{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", description='" + description + '\'' +
+                ", source='" + source + '\'' +
+                '}';
+    }
+}
